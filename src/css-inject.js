298174629e903df179b8450f4b0f60d6ad8f6836
Bluefinger <bluefinger@gmail.com>
2014-06-21 10:21:08 +0100

    Refactoring Code

--- src/css-inject.js
+++ src/css-inject.js
@@ -7,83 +7,78 @@
 	}
 }(this, function($){
 	"use strict";
-	var CssInject = function () {
-		var self = this;
-		if (!(self instanceof CssInject)) return new CssInject();
-		self.head = '';
-		self.elem = "css-inject-style";
-		self.styles = {};
-		self.noIE = false;
-	};
-	CssInject.fn = CssInject.prototype = {
-		parse : function () {
-			var css = "", selector, property, self = this;
-			for (selector in self.styles) {
-				if (self.styles.hasOwnProperty(selector)) {
+	var CssBase = function () {
+		var head;
+		this.parse = function () {
+			var css = "", selector, property;
+			for (selector in this.styles) {
+				if (this.styles.hasOwnProperty(selector)) {
 					css += selector + " {";
-					for (property in self.styles[selector]) {
-						if (self.styles[selector].hasOwnProperty(property)){
-							css += property + ":" + self.styles[selector][property] +";";
+					for (property in this.styles[selector]) {
+						if (this.styles[selector].hasOwnProperty(property)){
+							css += property + ":" + this.styles[selector][property] +";";
 						}
 					}
 					css += "} ";
 				}
 			}
 			return css;
-		},
-		
-		add : function (selector, property, value) {
-			var self = this;
-			if (!self.styles[selector]) { self.styles[selector] = {}; }
-			self.styles[selector][property] = value;
-			return self;
-		},
-		
-		objectAdd : function (collection) {
-			var self = this, i, l, selector, property;
+		};
+		this.add = function (selector, property, value) {
+			if (!this.styles[selector]) { this.styles[selector] = {}; }
+			this.styles[selector][property] = value;
+			return this;
+		};
+		this.objectAdd = function (collection) {
+			var selector, property;
 			if (typeof collection === "object") {
 				for (selector in collection) {
 					if (collection.hasOwnProperty(selector)){
 						for (property in collection[selector]) {
 							if (collection[selector].hasOwnProperty(property)){
-								self.add(selector, property, collection[selector][property]);
+								this.add(selector, property, collection[selector][property]);
 							}
 						}
 					}
 				}
 			}
-			return self;
-		},
-		
-		remove : function (selector, property) {
-			var self = this;
-			if (selector && property && self.styles.hasOwnProperty(property)) {
-				delete self.styles[selector][property];
-			} else if (selector && self.styles.hasOwnProperty(selector)) {
-				delete self.styles[selector];
+			return this;
+		};
+		this.remove = function (selector, property) {
+			if (selector && property && this.styles.hasOwnProperty(property)) {
+				delete this.styles[selector][property];
+			} else if (selector && this.styles.hasOwnProperty(selector)) {
+				delete this.styles[selector];
 			}
-			return self;
-		},
-		
-		apply : function () {
-			var css, text, self = this;
-			text = self.parse();
-			if (self.head && !self.noIE) {
-				css = '<style id="'+self.elem+'" type="text/css">'+text+'</style>';
-				self.head.replaceWith(css);
-				self.head = $('#'+self.elem);
-			} else if (self.head && self.noIE) {
-				self.head.html(text);
+			return this;
+		};
+		this.apply = function () {
+			var css, text;
+			text = this.parse();
+			if (head && !this.noIE) {
+				css = '<style id="'+this.elem+'" type="text/css">'+text+'</style>';
+				head.replaceWith(css);
+				head = $('#'+this.elem);
+			} else if (head && this.noIE) {
+				head.html(text);
 			} else {
 				if (text) {
-					css = '<style id="'+self.elem+'" type="text/css">'+text+'</style>';
+					css = '<style id="'+this.elem+'" type="text/css">'+text+'</style>';
 					$(css).appendTo('head');
-					self.head = $('#'+self.elem);
+					head = $('#'+this.elem);
 				}
 			}
-			return self;
-		}
+			return this;
+		};
+	},
+	
+	CssInject = function () {
+		this.elem = "css-inject-style";
+		this.styles = {};
+		this.noIE = false;
 	};
+	
+	CssInject.fn = CssInject.prototype = new CssBase();
 
-	return CssInject();
+	return new CssInject();
 }));
